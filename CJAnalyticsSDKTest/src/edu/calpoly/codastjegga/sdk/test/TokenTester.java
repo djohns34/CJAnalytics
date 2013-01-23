925df78f9fe500a9ee4b34bc3b33c0b616874ced
G <singhgagand11@gmail.com>
2013-01-22 20:27:08 -0800

    adding testing project for SDK.

---
+++ CJAnalyticsSDKTest/src/edu/calpoly/codastjegga/sdk/test/TokenTester.java
@@ -0,0 +1,48 @@
+package edu.calpoly.codastjegga.sdk.test;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+import edu.calpoly.codastjegga.sdk.Token;
+
+/**
+ * Test cases for Token
+ */
+
+public class TokenTester extends TestCase {
+	Token token;
+	final String accessToken = "Access-Token";
+	final String refreshToken = "Refresh-Token";
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		token = new Token(accessToken, refreshToken);
+	}
+	
+	@Test
+	public void testGetter(){
+	   assertEquals(accessToken, token.getAccessToken());
+	   assertEquals(refreshToken, token.getRefreshToken());
+	}
+	
+	@Test
+	public void testSetter(){
+	   String newAccessToken = "New-Access-Token";
+	   token.setAccessToken(newAccessToken);
+	   
+	    assertEquals(newAccessToken, token.getAccessToken());
+	}
+	
+	@Test
+	public void testToString(){
+		StringBuilder token= new StringBuilder();
+		token.append("{Token:");
+		token.append("    {Access Token:" + accessToken + 
+				          //",Refresh Token: " + refreshToken + 
+				          "}");
+		token.append("}");
+		
+	    assertEquals(token.toString(), token.toString());
+	}
+}
