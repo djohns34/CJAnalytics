3bc15346aaffcd5996921ba9afda3806c164a327
jrabaino <jeremy.rabaino@gmail.com>
2013-01-25 00:06:39 -0800

    Created blank application using Salesforce Template App with log ina and log out. CJ-1, CJ-2, CJ-35;

---
+++ CJAnalytics/gen/edu/calpoly/codastjegga/cjanalytics/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.calpoly.codastjegga.cjanalytics;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
