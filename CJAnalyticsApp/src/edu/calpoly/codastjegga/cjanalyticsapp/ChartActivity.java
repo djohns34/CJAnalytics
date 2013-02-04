b175104634058a57bf08472dae1302afbfa985f3
Corey Farwell <coreyf@rwell.org>
2013-02-03 23:22:31 -0800

    enable chart activity

--- CJAnalyticsApp/src/edu/calpoly/codastjegga/cjanalyticsapp/ChartActivity.java
+++ CJAnalyticsApp/src/edu/calpoly/codastjegga/cjanalyticsapp/ChartActivity.java
@@ -1,8 +1,11 @@
 package edu.calpoly.codastjegga.cjanalyticsapp;
 
+import org.achartengine.GraphicalView;
+
+import edu.calpoly.codastjegga.cjanalyticsapp.chart.CJPieChart;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.widget.LinearLayout;
 
 public class ChartActivity extends Activity {
 
@@ -10,5 +13,11 @@ public class ChartActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.chart);
+
+    CJPieChart pc = new CJPieChart();
+    GraphicalView view = pc.getView(this);
+
+    LinearLayout layout = (LinearLayout)findViewById(R.id.chart);
+    layout.addView(view);
   }
 }
---
+++ CJAnalyticsApp/src/edu/calpoly/codastjegga/cjanalyticsapp/chart/CJPieChart.java
@@ -0,0 +1,32 @@
+package edu.calpoly.codastjegga.cjanalyticsapp.chart;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.BarChart;
+import org.achartengine.model.CategorySeries;
+import org.achartengine.renderer.DefaultRenderer;
+import org.achartengine.renderer.SimpleSeriesRenderer;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+
+public class CJPieChart {
+  public GraphicalView getView(Context context) {
+    CategorySeries cs = new CategorySeries("my chart!");
+    DefaultRenderer ren = new DefaultRenderer();
+    SimpleSeriesRenderer ssr;
+    
+    cs.add(5);
+    ssr = new SimpleSeriesRenderer();
+    ssr.setColor(Color.GREEN);
+    ren.addSeriesRenderer(ssr);
+    
+    cs.add(20);
+    ssr = new SimpleSeriesRenderer();
+    ssr.setColor(Color.BLUE);
+    ren.addSeriesRenderer(ssr);
+    
+    return ChartFactory.getPieChartView(context, cs, ren);
+  }
+}
