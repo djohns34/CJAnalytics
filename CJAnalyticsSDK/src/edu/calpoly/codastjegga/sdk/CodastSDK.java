1d27b762f23bd148398b22c23317cae7fccd8803
Steven Silver <sasilver@calpoly.edu>
2013-02-08 13:38:55 -0800

    Filling in public methods and simplified the CodastSDK constructor.

--- CJAnalyticsSDK/src/edu/calpoly/codastjegga/sdk/CodastSDK.java
+++ CJAnalyticsSDK/src/edu/calpoly/codastjegga/sdk/CodastSDK.java
@@ -1,15 +1,33 @@
 package edu.calpoly.codastjegga.sdk;
 
+import android.app.Application;
+
 import com.salesforce.androidsdk.rest.RestClient;
+import com.salesforce.androidsdk.rest.RestClient.ClientInfo;
 
 
 public class CodastSDK implements ICodastSDK{
 	private RestClientAdapter restClientAdapter;
 	private RestClient client;
+	private SalesforceConnector connector;
 
-	public CodastSDK(RestClientAdapter restClientAdapter)
+	public CodastSDK(Application app, ClientInfo clientInfo, final Token token)
 	{
-		this.restClientAdapter = restClientAdapter;
+		this.restClientAdapter = new RestClientAdapter(app, clientInfo, token);
 		this.client = this.restClientAdapter.getRestClient();
+		
+		/*TODO: fill in the connector constructor */
+		this.connector = new SalesforceConnector(client, null, null);
+	}
+	
+	public void trackData(EventType type, String metric, Object data)
+	{
+		Event e = EventFactory.createEvent(type, metric, data);
+		connector.addEvent(e);
+	}
+	
+	public void sendData()
+	{
+		connector.sendEvents();
 	}
 }
--- CJAnalyticsSDK/src/edu/calpoly/codastjegga/sdk/ICodastSDK.java
+++ CJAnalyticsSDK/src/edu/calpoly/codastjegga/sdk/ICodastSDK.java
@@ -4,5 +4,6 @@ package edu.calpoly.codastjegga.sdk;
 public interface ICodastSDK {
 	public static final String APP_NAME = "CODAST-J3GGA-SDK";
 		
-	
+	public void trackData(EventType type, String metric, Object data);
+	public void sendData();
 }
