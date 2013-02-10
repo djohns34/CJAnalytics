cb882167e2b1f8da0e8c8c94a42cf69b6cf6a5b5
Corey Farwell <coreyf@rwell.org>
2013-02-10 10:54:44 -0800

    remove local files/dirs

--- CJAnalyticsAppTest/gen/edu/calpoly/codastjegga/cjanalyticsapp/test/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.calpoly.codastjegga.cjanalyticsapp.test;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
