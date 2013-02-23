2975e98343ca6def873881f894ac3de09ae429a2
Corey Farwell <coreyf@rwell.org>
2013-02-23 15:14:51 -0800

    remove add random graphs functionality

--- CJAnalyticsApp/src/edu/calpoly/codastjegga/cjanalyticsapp/StoredGraphsActivity.java
+++ CJAnalyticsApp/src/edu/calpoly/codastjegga/cjanalyticsapp/StoredGraphsActivity.java
@@ -1,16 +1,11 @@
 package edu.calpoly.codastjegga.cjanalyticsapp;
 
-import java.util.Random;
-
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import edu.calpoly.codastjegga.cjanalyticsapp.chart.ChartType;
 import edu.calpoly.codastjegga.cjanalyticsapp.chart.settings.ChartSettings;
 import edu.calpoly.codastjegga.cjanalyticsapp.chart.settings.ChartSettingsProvider;
 import edu.calpoly.codastjegga.cjanalyticsapp.dashboard.Dashboard;
@@ -45,34 +40,6 @@ public class StoredGraphsActivity extends GraphsActivity{
     // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.activity_graphs, menu);
     return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    testAdd();
-    testAdd();
-    switch (item.getItemId()) {
-    case android.R.id.home:
-      // This ID represents the Home or Up button. In the case of this
-      // activity, the Up button is shown. Use NavUtils to allow users
-      // to navigate up one level in the application structure. For
-      // more details, see the Navigation pattern on Android Design:
-      //
-      // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-      //
-      NavUtils.navigateUpFromSameTask(this);
-      return true;
-    }
-    return super.onOptionsItemSelected(item);
-  }
-    
-  //TODO Remove Me!!
-  public void testAdd(){
-    ChartSettings testSetting=new ChartSettings(ChartType.values()[new Random().nextInt(3)], "Temple Run","Graph Name", "Wage", null, null);
-    
-    /*Insert the test setting*/
-    
-    ChartSettingsProvider.saveSettings(getContentResolver(), testSetting);
   }
 
  
