eaba28722fbbe933a25be396ecfd1dbb268139f3
Corey Farwell <coreyf@rwell.org>
2013-02-26 23:07:57 -0800

    pie chart now uses actual data

--- CJAnalyticsApp/src/edu/calpoly/codastjegga/cjanalyticsapp/chart/CJPieChart.java
+++ CJAnalyticsApp/src/edu/calpoly/codastjegga/cjanalyticsapp/chart/CJPieChart.java
@@ -1,6 +1,9 @@
 package edu.calpoly.codastjegga.cjanalyticsapp.chart;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
@@ -12,23 +15,36 @@ import edu.calpoly.codastjegga.cjanalyticsapp.event.Event;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 
-public class CJPieChart implements ChartProvider{
+public class CJPieChart implements ChartProvider {
   public GraphicalView getGraphicalView(Context context, List<Event> events) {
-    CategorySeries cs = new CategorySeries("my chart!");
+    CategorySeries cs = new CategorySeries("Chart Title Here");
     DefaultRenderer ren = new DefaultRenderer();
     SimpleSeriesRenderer ssr;
-    
-    cs.add(5);
-    ssr = new SimpleSeriesRenderer();
-    ssr.setColor(Color.GREEN);
-    ren.addSeriesRenderer(ssr);
-    
-    cs.add(20);
-    ssr = new SimpleSeriesRenderer();
-    ssr.setColor(Color.BLUE);
-    ren.addSeriesRenderer(ssr);
-    
+    Random rand = new Random();
+    HashMap<String, Integer> values = new HashMap<String, Integer>();
+
+    // TODO: Need a way to get
+    ren.setChartTitle("Chart Title Here");
+
+    for (Event e : events) {
+      String curr = e.getValue().toString();
+      if (values.containsKey(curr)) {
+        values.put(curr, values.get(curr) + 1);
+      } else {
+        values.put(curr, 1);
+      }
+    }
+
+    for (Map.Entry<String, Integer> entry : values.entrySet()) {
+      Log.d(entry.getKey(), entry.getValue().toString());
+      cs.add(entry.getKey(), entry.getValue());
+      ssr = new SimpleSeriesRenderer();
+      ssr.setColor(Color.rgb(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256)));
+      ren.addSeriesRenderer(ssr);
+    }
+
     return ChartFactory.getPieChartView(context, cs, ren);
   }
 }
