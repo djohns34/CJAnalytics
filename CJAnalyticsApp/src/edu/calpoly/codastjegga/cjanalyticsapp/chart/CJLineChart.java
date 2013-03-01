3ac0aa6e4117ee801a502ee2cc0e9e6ee74a9e6c
Corey Farwell <coreyf@rwell.org>
2013-03-01 12:53:16 -0800

    line charts now use live data

--- CJAnalyticsApp/src/edu/calpoly/codastjegga/cjanalyticsapp/chart/CJLineChart.java
+++ CJAnalyticsApp/src/edu/calpoly/codastjegga/cjanalyticsapp/chart/CJLineChart.java
@@ -1,59 +1,72 @@
 package edu.calpoly.codastjegga.cjanalyticsapp.chart;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
+import org.achartengine.model.TimeSeries;
 import org.achartengine.model.XYMultipleSeriesDataset;
-import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 
+import android.content.Context;
+import android.graphics.Color;
 import edu.calpoly.codastjegga.cjanalyticsapp.chart.settings.ChartSettings;
 import edu.calpoly.codastjegga.cjanalyticsapp.event.Event;
 
-import android.content.Context;
-import android.graphics.Color;
-
 public class CJLineChart implements ChartProvider {
+
+  XYMultipleSeriesDataset data;
+  XYMultipleSeriesRenderer ren;
+
   public void parseData(ChartSettings chartSettings, List<Event> events) {
-
-  }
-
-  public GraphicalView getGraphicalView(Context context) {
-    XYMultipleSeriesDataset data = new XYMultipleSeriesDataset();
-    XYMultipleSeriesRenderer ren = new XYMultipleSeriesRenderer();
-    
-    ChartRendererSetting.appendCustomRendererSetting(ren);
-    
     XYSeriesRenderer xysr;
-    XYSeries s;
+    TimeSeries timeSeries = new TimeSeries("My Events");
+    HashMap<Date, LinkedList<Double>> values = new HashMap<Date, LinkedList<Double>>();
 
-    s = new XYSeries("Blah 1");
-    s.add(1, 10);
-    data.addSeries(s);
-    ren.addXTextLabel(1, s.getTitle());
+    data = new XYMultipleSeriesDataset();
+    ren = new XYMultipleSeriesRenderer();
+
+    // If there's an event that occurs at the same time as another event,
+    // average the values
+
+    // map from timestamp to a list of all values at that time
+    for (Event event : events) {
+      Double value = Double.parseDouble(event.getValue().toString()); // TODO: this is a hack
+      Date time = event.getTimestamp();
+
+      if (values.containsKey(time)) {
+        values.get(time).add(value);
+      } else {
+        values.put(time, new LinkedList<Double>());
+        values.get(time).add(value);
+      }
+    }
+
+    // calculate the average value for that timestamp, add to the time series
+    for (Map.Entry<Date, LinkedList<Double>> entries : values.entrySet()) {
+      Double sum = Double.valueOf(0);
+
+      for (Double entry : entries.getValue()) {
+        sum += entry;
+      }
+      sum /= entries.getValue().size();
+
+      timeSeries.add(entries.getKey(), sum);
+    }
+
+    data.addSeries(timeSeries);
 
     xysr = new XYSeriesRenderer();
     xysr.setColor(Color.GREEN);
     ren.addSeriesRenderer(xysr);
+  }
 
-    s = new XYSeries("Blah 2");
-    s.add(2, 15);
-    data.addSeries(s);
-    ren.addXTextLabel(2, s.getTitle());
-
-    xysr = new XYSeriesRenderer();
-    xysr.setColor(Color.BLUE);
-    ren.addSeriesRenderer(xysr);
-
-    ren.setXLabels(0);
-    ren.setBarSpacing(.5);
-    ren.setYAxisMin(0);
-    ren.setXAxisMin(0);
-
-    ren.setMarginsColor(Color.argb(0, 255, 255, 255));
-
-    return ChartFactory.getLineChartView(context, data, ren);
+  public GraphicalView getGraphicalView(Context context) {
+    return ChartFactory.getTimeChartView(context, data, ren, "MM/dd/yyyy");
   }
 }
