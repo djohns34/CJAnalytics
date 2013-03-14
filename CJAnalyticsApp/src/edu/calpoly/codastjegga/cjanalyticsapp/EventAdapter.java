d12c7d07f80b1d3a7031bc0376d201ddb2ddac4d
G <singhgagand11@gmail.com>
2013-03-14 06:42:00 -0700

    forgot to merge EventAdapter

--- CJAnalyticsApp/src/edu/calpoly/codastjegga/cjanalyticsapp/EventAdapter.java
+++ CJAnalyticsApp/src/edu/calpoly/codastjegga/cjanalyticsapp/EventAdapter.java
@@ -24,16 +24,8 @@ import edu.calpoly.codastjegga.cjanalyticsapp.event.EventType;
  */
 public class EventAdapter extends BaseAdapter{
 
-<<<<<<< HEAD
   //List of events
   List<Map.Entry<String, EventType>> eventsList;
-=======
-  //list of metrics
-  ArrayList<String> eventMetrics;
-  //list of event types
-  ArrayList<EventType> eventTypes;
-
->>>>>>> b66f3c8594464c70928ef54bf95ac8d1469adbdb
   //context to link to the parent activity
   Context context;
 
@@ -50,35 +42,19 @@ public class EventAdapter extends BaseAdapter{
    * @param context
    * @param eventsList
    */
-<<<<<<< HEAD
+
   public EventAdapter(Context context, List<Map.Entry<String, EventType>> eventsList){
-=======
-  public EventAdapter(Context context, ArrayList<String> eventsMetrics, ArrayList<EventType> eventTypes){
->>>>>>> b66f3c8594464c70928ef54bf95ac8d1469adbdb
     this.context = context;
-    this.eventMetrics = eventsMetrics;
-    this.eventTypes = eventTypes;
+    this.eventsList = eventsList;
   }
 
-  /**
-   * Getter for Events List
-   * @return list of metrics
-   */
-  public ArrayList<String> getMetricsList() {
-    return eventMetrics;
-  }
 
   /**
    * Getter for Event types List
    * @return list of event types
    */
-<<<<<<< HEAD
   public List<Map.Entry<String, EventType>> getEventsList() {
     return eventsList;
-=======
-  public ArrayList<EventType> getEventTypeList() {
-    return eventTypes;
->>>>>>> b66f3c8594464c70928ef54bf95ac8d1469adbdb
   }
 
   /**
@@ -86,14 +62,9 @@ public class EventAdapter extends BaseAdapter{
    * @param eventsList list of events 
    * @param eventsTypes list of event types
    */
-<<<<<<< HEAD
   public void setEventsList(List<Map.Entry<String, EventType>> eventsList) {
     this.eventsList = eventsList;
-=======
-  public void setEventsList(ArrayList<String> eventsMetrics, ArrayList<EventType> eventsTypes) {
-    this.eventMetrics = eventsMetrics;
-    this.eventTypes = eventsTypes;
->>>>>>> b66f3c8594464c70928ef54bf95ac8d1469adbdb
+
   }
 
   /**
@@ -123,7 +94,6 @@ public class EventAdapter extends BaseAdapter{
 
     TextView metricName = (TextView) view.findViewById(R.id.metric_name);
     TextView metricType = (TextView) view.findViewById(R.id.metric_type);
-<<<<<<< HEAD
     
     if (eventsList != null) {
      Map.Entry<String, EventType> metric = eventsList.get(position);
@@ -131,15 +101,7 @@ public class EventAdapter extends BaseAdapter{
     metricName.setText(metric.getKey());
     metricType.setText(metric.getValue().toString());
     }
-=======
 
-
-    if (eventMetrics != null && eventTypes != null) {
-      metricName.setText(this.eventMetrics.get(position));
-      metricType.setText(this.eventTypes.get(position).toString());
-    }
-
->>>>>>> b66f3c8594464c70928ef54bf95ac8d1469adbdb
     return view;
   }
   /**
@@ -147,15 +109,10 @@ public class EventAdapter extends BaseAdapter{
    */
   @Override
   public int getCount() {
-<<<<<<< HEAD
     if (eventsList == null)
       return 0;
    return eventsList.size();
-=======
-    if (eventMetrics == null) return 0; 
-    
-    return eventMetrics.size();
->>>>>>> b66f3c8594464c70928ef54bf95ac8d1469adbdb
+
   }
 
   /**
@@ -164,9 +121,9 @@ public class EventAdapter extends BaseAdapter{
    */
   @Override
   public Object getItem(int position) {
-    if (eventMetrics == null)
+    if (eventsList == null)
       return null;
-    return Pair.create(eventMetrics.get(position), eventTypes.get(position));
+    return eventsList.get(position);
   }
 
   /**
@@ -185,12 +142,7 @@ public class EventAdapter extends BaseAdapter{
    * @param metric metric name
    * @return position of the event
    */
-<<<<<<< HEAD
   public int getPosition (Map.Entry<String, EventType> eventsList) {
       return this.eventsList.indexOf(eventsList);
-=======
-  public int getPosition (String metric) {
-    return this.eventMetrics.indexOf(metric);
->>>>>>> b66f3c8594464c70928ef54bf95ac8d1469adbdb
   }
 }
