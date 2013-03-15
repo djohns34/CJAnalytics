fb8d0ba838db9fef77b749b8a60d523de34f12b4
Corey Farwell <coreyf@rwell.org>
2013-03-15 10:40:30 -0700

    add the rest of the input fields for the types

--- ButtonApp/src/com/example/buttonapp/MainActivity.java
+++ ButtonApp/src/com/example/buttonapp/MainActivity.java
@@ -18,12 +18,12 @@ public class MainActivity extends Activity {
   static final String api = "v23.0";
   static final String loginInstance = "https://na9.salesforce.com";
   static final Token token = new Token(
-      "00DE0001000dgAF!AQwAQIpSdhxe_ncKl_4bN2cc8q.e89gqxk0Julhd4SiZszOxupA.r12tkcbejBRWixyuvKOX5zDJVQ_sThHcQpRAmjHSJSFo",
-      "5Aep861rEpScxnNE64IogmmXsb72ONfi9xneXYd7423HJqVESg_uE.dE.85z.8DhrhkoQed5DSFKw==");
+      "00DE0000000dgAF!AQwAQHnV8ok8IFGhQg8bFaLM.WV.CR2BDLWZZCh_qytFixalDib.5j5SOPX6S6qCSXmRyF1NtotWqhdWzEmo5uyl.Hr4R0lm",
+      "5Aep861rEpScxnNE64IogmmXsb72ONfi9xneXYd7423HJqVESjLhLrL2PgSgS6FwFkIJGlh8KbLnQ==");
   static final String clientId = "3MVG9y6x0357HlefAMuoGiOstBQg3r5rHpEodDBNQ6GIjd5Fh.RPRX4YA6Ax_sg69Jq8tpuFU76NlE0z5LTbc";
 
   CodastSDK sdk;
-  EditText stringField, floatField;
+  EditText stringField, floatField, integerField, currencyField, localeField;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -32,6 +32,9 @@ public class MainActivity extends Activity {
 
     stringField = (EditText) findViewById(R.id.editText1);
     floatField = (EditText) findViewById(R.id.editText2);
+    integerField = (EditText) findViewById(R.id.editText3);
+    currencyField = (EditText) findViewById(R.id.editText4);
+    localeField = (EditText) findViewById(R.id.editText5);
 
     try {
       sdk = new CodastSDK(getApplication(), loginInstance, token, "Button-App",
@@ -63,5 +66,28 @@ public class MainActivity extends Activity {
     Float value = Float.parseFloat(floatField.getText().toString());
     sdk.trackData(EventType.Float, "my-floats", value);
     sdk.sendData();
+  }
+
+  public void onSendIntegerEventClick(View view) {
+    Integer value = Integer.parseInt(integerField.getText().toString());
+    sdk.trackData(EventType.Integer, "my-integers", value);
+    sdk.sendData();
+  }
+
+  public void onSendCurrencyEventClick(View view) {
+    Double value = Double.parseDouble(currencyField.getText().toString());
+    sdk.trackData(EventType.Currency, "my-currencies", value);
+    sdk.sendData();
+  }
+  
+  public void onSendLocaleEventClick(View view) {
+    String value = localeField.getText().toString();
+    if (value.length() > 0) {
+      sdk.trackData(EventType.Text, "my-locales", value);
+      sdk.sendData();
+    } else {
+      Toast.makeText(getApplicationContext(), "No Locale inputted",
+          Toast.LENGTH_SHORT).show();
+    }
   }
 }
