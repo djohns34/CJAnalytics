c66c5687eb3ebc4a95b78864d61d2dd119ca445c
Corey Farwell <coreyf@rwell.org>
2013-03-15 12:49:09 -0700

    add prettier style to the bar charts

--- CJAnalyticsApp/src/edu/calpoly/codastjegga/cjanalyticsapp/chart/CJBarChart.java
+++ CJAnalyticsApp/src/edu/calpoly/codastjegga/cjanalyticsapp/chart/CJBarChart.java
@@ -12,12 +12,14 @@ import org.achartengine.model.XYMultipleSeriesDataset;
 import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.SimpleSeriesRenderer;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
 
 import edu.calpoly.codastjegga.cjanalyticsapp.chart.settings.ChartSettings;
 import edu.calpoly.codastjegga.cjanalyticsapp.event.Event;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Paint.Align;
 
 public class CJBarChart implements ChartProvider {
   XYMultipleSeriesDataset data;
@@ -26,14 +28,35 @@ public class CJBarChart implements ChartProvider {
   public void parseData(ChartSettings chartSettings, List<Event> events) {
     data = new XYMultipleSeriesDataset();
     ren = new XYMultipleSeriesRenderer();
-    ChartRendererSetting.appendCustomRendererSetting(ren);
-    
+    double panLimits[] = {0, Double.MAX_VALUE, 0, Double.MAX_VALUE};
     XYSeries xySeries;
-    SimpleSeriesRenderer ssr;
+    XYSeriesRenderer xysr;
     Random rand = new Random();
     HashMap<String, Integer> values = new HashMap<String, Integer>();
     int xIndex = 1;
-
+    
+    ChartRendererSetting.appendCustomRendererSetting(ren);
+    ren.setPanLimits(panLimits);
+    
+    // Axes
+    ren.setAxesColor(Color.BLACK);
+    
+    // X
+    ren.setXLabels(0);
+    
+    // Y
+    ren.setYLabelsColor(0, Color.BLACK);
+    ren.setYLabelsAlign(Align.LEFT);
+    ren.setYTitle("Number of occurences");
+    ren.setAxisTitleTextSize(ChartRendererSetting.TEXT_SIZE);
+    
+    // Grid
+    ren.setShowGridX(true);
+    ren.setGridColor(Color.argb(255, 200, 200, 200));
+    
+    // Margin
+    ren.setMarginsColor(Color.argb(0, 255, 255, 255));
+    
     for (Event e : events) {
       String curr = e.getValue().toString();
       if (values.containsKey(curr)) {
@@ -49,20 +72,14 @@ public class CJBarChart implements ChartProvider {
       data.addSeries(xySeries);
       ren.addXTextLabel(xIndex, xySeries.getTitle());
 
-      ssr = new SimpleSeriesRenderer();
-      ssr.setColor(Color.rgb(rand.nextInt(256), rand.nextInt(256),
+      xysr = new XYSeriesRenderer();
+      xysr.setColor(Color.rgb(rand.nextInt(256), rand.nextInt(256),
           rand.nextInt(256)));
-      ren.addSeriesRenderer(ssr);
+      xysr.setLineWidth(20f);
+      ren.addSeriesRenderer(xysr);
 
       xIndex += 1;
     }
-
-    ren.setXLabels(0);
-    ren.setBarSpacing(.5);
-    ren.setYAxisMin(0);
-    ren.setXAxisMin(0);
-
-    ren.setMarginsColor(Color.argb(0, 255, 255, 255));
   }
 
   public GraphicalView getGraphicalView(Context context) {
